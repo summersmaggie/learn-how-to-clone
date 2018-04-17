e9fd5c601c34614f4153162d24ad1c22cedea233
Hannah Lee and Maggie Summers <maggie@>
2018-04-17 16:29:31 -0700

    try to get integration spec passing

--- spec/features/click_section_title_spec.rb
+++ spec/features/click_section_title_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe "the action of clicking a section" do
   it "shows routes to list of lessons for section" do
     visit '/'
+    binding.pry
+    save_and_open_page
     click_link 'Ruby'
     expect(page).to have_content 'Sinatra'
   end
