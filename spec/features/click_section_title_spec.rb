c60b26f550587bfbd4b4dd31dc04b16c55fb94f2
Hannah Lee and Maggie Summers <maggie@>
2018-04-17 16:25:23 -0700

    render chapter text and update integration test path for visit.

--- spec/features/click_section_title_spec.rb
+++ spec/features/click_section_title_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "the action of clicking a section" do
   it "shows routes to list of lessons for section" do
-    visit sections_path(section)
+    visit '/'
     click_link 'Ruby'
     expect(page).to have_content 'Sinatra'
   end
