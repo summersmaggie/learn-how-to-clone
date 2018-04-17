2fb738a34c413103c843522834e87eaa9f74ea7f
Hannah Lee and Maggie Summers <maggie@>
2018-04-17 15:41:23 -0700

    add route from section to chapter

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,7 +1,8 @@
 class ChaptersController < ApplicationController
   def index
-
     @lesson = Lesson.find(params[:lesson_id])
     @chapters = @lesson.chapters.all
+    @section = @lesson.section
+    @chapter = Chapter.find(params[:lesson_id])
   end
 end
