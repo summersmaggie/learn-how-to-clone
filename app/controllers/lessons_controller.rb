8497d08a47106edadefbc2c89c17f2637388ea8b
Hannah Lee and Maggie Summers <maggie@>
2018-04-17 11:22:51 -0700

    fix routing to link sections to lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,6 @@
 class LessonsController < ApplicationController
   def index
     @section = Section.find(params[:section_id])
-    @lesson = @section.lessons.all
+    @lessons = @section.lessons.all
   end
 end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,5 +1,15 @@
 class SectionsController < ApplicationController
+  # def index
+  #   @sections = Section.all
+  #
+  #   @lesson = Lesson.find(params[:section_id])
+  #   @lessons = @section.lessons.all
+  #
+  # end
+
   def index
     @sections = Section.all
+    #  binding.pry
+    # @section = Section.find(params[:id])
   end
 end
