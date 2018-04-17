efdb7da987b77d576239d9a724bcd6a55f85ce07
Hannah Lee and Maggie Summers <maggie@>
2018-04-17 10:37:30 -0700

    add list of lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,6 @@
 class Lesson < ActiveRecord::Base
+  belongs_to :section
+
   validates :lesson_title, :presence => true
   validates :number, :presence => true
   validates :section_id, :presence => true
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,5 @@
 class Section < ActiveRecord::Base
+  has_many :lessons
+
   validates :section_title, :presence => true
 end
