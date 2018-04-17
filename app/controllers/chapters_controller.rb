f4d65a853b676081913dc85c1a4cc6ecd6018c4c
Hannah Lee and Maggie Summers <maggie@>
2018-04-17 15:54:36 -0700

    add links to chapters

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -5,4 +5,12 @@ class ChaptersController < ApplicationController
     @section = @lesson.section
     @chapter = Chapter.find(params[:lesson_id])
   end
+
+  def show
+    @lesson = Lesson.find(params[:lesson_id])
+    @chapters = @lesson.chapters.all
+    @section = @lesson.section
+    @chapter = Chapter.find(params[:lesson_id])
+  end
+
 end
