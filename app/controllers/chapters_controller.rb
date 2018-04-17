3e235967ac4ea5f96cb0d0b0b7287c055d1e944d
Hannah Lee and Maggie Summers <hannah@>
2018-04-17 16:08:27 -0700

    add link through to show chapter view

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -7,10 +7,11 @@ class ChaptersController < ApplicationController
   end
 
   def show
-    @lesson = Lesson.find(params[:lesson_id])
-    @chapters = @lesson.chapters.all
-    @section = @lesson.section
+    # @lesson = Lesson.find(params[:lesson_id])
+    # @chapters = @lesson.chapters.all
+    # @section = @lesson.section
     @chapter = Chapter.find(params[:lesson_id])
+    render :show
   end
 
 end
