06c166180290385865060a899796a494d8a8a808
Kaila Goff and Maggie Summers <maggie@>
2018-04-18 10:03:06 -0700

    add nesting for Faker on lesson and chapter models

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,23 @@ require 'faker'
 
 Section.destroy_all
 Lesson.destroy_all
+Chapter.destroy_all
 
-20.times do |index|
+7.times do |index|
   @section = Section.create!(section_title: Faker::ProgrammingLanguage.name)
-    10.times do
-      Lesson.create!(lesson_title: Faker::ProgrammingLanguage.name,
+    5.times do
+      @lesson = Lesson.create!(lesson_title: Faker::ProgrammingLanguage.name,
                       section_id: @section.id,
                       number: Faker::Number.between(1, 10))
+    5.times do
+      Chapter.create!(title: Faker::Lorem.words(4),
+        text: Faker::Lorem.paragraphs(3),
+        section_id: @section.id,
+        lesson_id: @lesson.id)
+      end
     end
 end
 
 p "Created #{Section.count} sections"
 p "Created #{Lesson.count} lessons"
+p "Created #{Chapter.count} chapters"
