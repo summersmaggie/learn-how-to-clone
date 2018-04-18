438d2c9063350be309ff2bea59cc4299b3c38aca
Kaila Goff and Maggie Summers <maggie@>
2018-04-18 09:41:04 -0700

    add nested seeding with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Section.destroy_all
+Lesson.destroy_all
+
+20.times do |index|
+  @section = Section.create!(section_title: Faker::ProgrammingLanguage.name)
+    10.times do
+      Lesson.create!(lesson_title: Faker::ProgrammingLanguage.name,
+                      section_id: @section.id,
+                      number: Faker::Number.between(1, 10))
+    end
+end
+
+p "Created #{Section.count} sections"
+p "Created #{Lesson.count} lessons"
